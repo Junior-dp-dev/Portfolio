59023dc58271e547c94ccf01c7ea914c819f8ba5
Junior <junior.dp.dev@gmail.com>
2023-02-22 09:42:12 -0300

    Ajuste para telas menores, ajuste na section about

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ document.removeEventListener("mouseover", (event) => mouseEnterHandle(event));
 document.removeEventListener("mouseout", (event) => mouseOutHandle(event));
 
 /* Configuração do Card das Habilidades*/
-const cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll(".card-skill");
 const cardHidden = document.querySelector(".card-hidden");
 const cardHiddenTitle = cardHidden.querySelector(".title");
 const cardHiddenText = cardHidden.querySelector(".text");
@@ -64,3 +64,41 @@ cards.forEach((card) => {
 cardHidden.addEventListener("click", () => {
   cardHidden.style.display = "none";
 });
+
+//Slider
+const slider = document.querySelectorAll(".slider");
+const btnPrev = document.getElementById("prev-button");
+const btnNext = document.getElementById("next-button");
+
+let currentSlide = 0;
+
+function hideSlider() {
+  slider.forEach((item) => item.classList.remove("on"));
+}
+
+function showSlider() {
+  slider[currentSlide].classList.add("on");
+}
+
+function nextSlider() {
+  hideSlider();
+  if (currentSlide === slider.length - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+  showSlider();
+}
+
+function prevSlider() {
+  hideSlider();
+  if (currentSlide === 0) {
+    currentSlide = slider.length - 1;
+  } else {
+    currentSlide--;
+  }
+  showSlider();
+}
+
+btnNext.addEventListener("click", nextSlider);
+btnPrev.addEventListener("click", prevSlider);
